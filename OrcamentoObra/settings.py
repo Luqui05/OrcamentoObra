c86aa7764f93bedcf3247c4830cfb3637eccdafc
Lucas Ribeiro <82158676+Luqui05@users.noreply.github.com>
2026-05-14 23:25:50 -0300

    feat: config inicial do crispy forms

--- OrcamentoObra/settings.py
+++ OrcamentoObra/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "core.apps.CoreConfig",
     "usuarios.apps.UsuariosConfig",
 ]
@@ -172,3 +174,7 @@ LOGOUT_REDIRECT_URL = "index"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Crispy Forms (usar bootstrap5)
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
